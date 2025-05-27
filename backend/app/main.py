386f987f6a7caf10fa2bfe7e969b7f657ca186da
FunDuckich <petrovegor495@gmail.com>
2025-05-27 20:14:36 +0400

    правочки

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from .routers import auth_router as auth, products_router as products, categories_router as categories
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth, prefix="/auth", tags=["auth"])
 app.include_router(products, prefix="/products", tags=["products"])
--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from ..database import get_db
 from ..models.user import User as UserModel
-from ..schemas.user import UserCreate, User
-from ..utils import get_password_hash, create_access_token, authenticate_user
+from ..schemas.user import UserCreate, User as UserSchema
+from ..utils import verify_password, get_password_hash, create_access_token, authenticate_user, \
+    get_current_active_user
 from datetime import timedelta, date
 
 router = APIRouter()
@@ -27,22 +28,25 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSessi
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@router.post("/register", response_model=User)
+@router.post("/register", response_model=UserSchema)
 async def register(user: UserCreate, db: AsyncSession = Depends(get_db)):
     stmt = select(UserModel).where(UserModel.client_name == user.client_name)
     result = await db.execute(stmt)
     db_user = result.scalars().first()
-
     if db_user:
         raise HTTPException(status_code=400, detail="Username already registered")
-
     hashed_password = get_password_hash(user.password)
-    new_user = UserModel(
+    db_user = UserModel(
         client_name=user.client_name,
         hashed_password=hashed_password,
         registration_date=date.today()
     )
-    db.add(new_user)
+    db.add(db_user)
     await db.commit()
-    await db.refresh(new_user)
-    return new_user
+    await db.refresh(db_user)
+    return db_user
+
+
+@router.get("/me", response_model=UserSchema)
+async def get_current_user_info(current_user: UserModel = Depends(get_current_active_user)):
+    return current_user
